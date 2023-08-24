852bc59cb010d87eb2a996fd8dd19ea0ca3673ad
100sang <jominsang@gmail.com>
2023-08-25 03:08:24 +0900

    error test

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ const routes = [
   },
   {
     path: '/demo',   name: 'Demo',
-    component: () => import('@/views/Demo')
+    component: () => import('@/views/Test')
   },
 ]
 
---
+++ src/views/Test.vue
@@ -0,0 +1,13 @@
+<template>
+  <h5>Error Check!</h5>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
